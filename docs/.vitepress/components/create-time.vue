8531b4b47321d52c53da4384947cf51eed00be2e
轩小浅 <3102535247@qq.com>
2025-06-13 23:51:45 +0800

    新增文章创建日期组件

---
+++ docs/.vitepress/components/create-time.vue
@@ -0,0 +1,33 @@
+<template>
+  <div v-if="frontmatter.createTime" style="
+        line-height: 32px;
+        font-size: 14px;
+        font-weight: 500;
+        margin-bottom: 20px;
+        float: right;">
+    创建时间: {{ formatDate(frontmatter.createTime) }}
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useData } from 'vitepress';
+
+function formatDate(inputStr) {
+  // 提取日期时间各部分
+  const matches = inputStr.match(/(\d+)年(\d+)月(\d+)日(\d+):(\d+):(\d+)/)
+  if (!matches) return inputStr
+
+  const [_, year, month, day, hour, minute] = matches
+
+  // 构造新格式
+  return `${year}/${parseInt(month)}/${parseInt(day)} ${hour}:${minute}`
+}
+
+
+const { frontmatter, page } = useData()
+
+console.log(frontmatter.value.createTime);
+
+</script>
+
+<style lang="scss" scoped></style>
--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -2,12 +2,14 @@ import 'viewerjs/dist/viewer.min.css'
 import { useRoute } from 'vitepress'
 import imageViewer from 'vitepress-plugin-image-viewer'
 import Theme from "vitepress/theme"
+import CreateTime from '../components/create-time.vue'
 import "./style/article.css"
 import "./style/var.css"
 
 export default {
   ...Theme,
   enhanceApp({ app }) {
+    app.component('CreateTime', CreateTime)
   },
   setup() {
     // 获取路由
