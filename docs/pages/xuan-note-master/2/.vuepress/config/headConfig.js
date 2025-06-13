3dd4a6534a4a1ba4d4884084392d2af282364ee5
轩小浅 <3102535247@qq.com>
2025-06-13 23:23:38 +0800

    写了两篇文章，修补了错误样式

--- docs/pages/xuan-note-master/2/.vuepress/config/headConfig.js
+++
@@ -1,5 +0,0 @@
-module.exports = [
-    ['link', { rel: 'icon', href: '/image/xuanxiaoqian.jpg' }],
-    ['meta', { name: 'author', content: '轩小浅' }],
-    ['meta', { name: 'keywords', content: '个人笔记,轩小浅,个人博客' }],
-]
