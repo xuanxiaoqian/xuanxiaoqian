e04f73844c119ab026bccbb5b9c67bce96ed1d0b
轩小浅 <3102535247@qq.com>
2025-06-15 23:18:37 +0800

    update base

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -7,7 +7,7 @@ import { navConfig } from './config/nav'
 import { sidebarConfig } from './config/sidebar'
 
 export default defineConfig({
-  base: 'xuanxiaoqian',
+  base: '/xuanxiaoqian/',
   ignoreDeadLinks: true,
   title: '轩小浅',
   description: '轩小浅官网',
