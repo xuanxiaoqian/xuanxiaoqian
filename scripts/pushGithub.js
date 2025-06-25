c2fd07bf11a2cdcaf460303cf303084583b125b7
xuanxiaoqian <3102535247@qq.com~>
2025-06-25 14:26:02 +0800

    优化sh脚本兼容问题

--- scripts/pushGithub.js
+++ scripts/pushGithub.js
@@ -1,17 +1,19 @@
 #!/usr/bin/env zx
+import { execSync } from 'child_process';
 import 'zx/globals';
 
-const now = new Date();
-const formattedTime = `${now.getFullYear()}-${(now.getMonth()+1).toString().padStart(2,'0')}-${now.getDate().toString().padStart(2,'0')} ${now.getHours().toString().padStart(2,'0')}:${now.getMinutes().toString().padStart(2,'0')}:${now.getSeconds().toString().padStart(2,'0')}`;
-
-await $`npm run build`;
-
-// 更好的目录切换方式
-await $`git -C ./docs/.vitepress/dist add .`;
-await $`git -C ./docs/.vitepress/dist commit -m "提交时间：${formattedTime}"`;
-
 try {
-  await $`git -C ./docs/.vitepress/dist push -f github pages`;
+
+  const now = new Date();
+  const formattedTime = `${now.getFullYear()}-${(now.getMonth() + 1).toString().padStart(2, '0')}-${now.getDate().toString().padStart(2, '0')} ${now.getHours().toString().padStart(2, '0')}:${now.getMinutes().toString().padStart(2, '0')}:${now.getSeconds().toString().padStart(2, '0')}`;
+
+  execSync(`npm run build`);
+
+  execSync(`git -C ./docs/.vitepress/dist add .`);
+  execSync(`git -C ./docs/.vitepress/dist commit -m "提交时间：${formattedTime}"`);
+  execSync(`git -C ./docs/.vitepress/dist push -f github pages`);
+
 } catch (error) {
   console.error('推送失败:', error);
+
 }
